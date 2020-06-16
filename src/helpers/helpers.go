9bd380112ebe74d4849f2c11a49de7a430e53d7e
Will Rowe <w.p.m.rowe@gmail.com>
2020-06-16 14:27:30 +0100

    adding in ipv4 processing

--- src/helpers/helpers.go
+++ src/helpers/helpers.go
@@ -58,20 +58,17 @@ func IsPublicIPv4(ip net.IP) bool {
 	if ip.IsLoopback() || ip.IsLinkLocalMulticast() || ip.IsLinkLocalUnicast() {
 		return false
 	}
-
-	/*
-		if ip4 := ip.To4(); ip4 != nil {
-			switch true {
-			case ip4[0] == 10:
-				return false
-			case ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31:
-				return false
-			case ip4[0] == 192 && ip4[1] == 168:
-				return false
-			default:
-				return true
-			}
+	if ip4 := ip.To4(); ip4 != nil {
+		switch true {
+		case ip4[0] == 10:
+			return false
+		case ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31:
+			return false
+		case ip4[0] == 192 && ip4[1] == 168:
+			return false
+		default:
+			return true
 		}
-	*/
+	}
 	return true
 }
